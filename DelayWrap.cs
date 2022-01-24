69e1fe6b96a7a64702e0400f6f2866045e906351
SoulReborn2021 <95975543+SoulReborn2021@users.noreply.github.com>
2022-01-24 09:56:29 +0800

    DelayWrap

---
+++ DelayWrap.cs
@@ -0,0 +1,173 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class DelayWrap
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			Utils.BeginObjectRegister(typeof(Delay), L, translator, 0, 1, 2, 2);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "ResetDelay", _m_ResetDelay);
+			
+			
+			Utils.RegisterFunc(L, Utils.GETTER_IDX, "delayTime", _g_get_delayTime);
+            Utils.RegisterFunc(L, Utils.GETTER_IDX, "seted", _g_get_seted);
+            
+			Utils.RegisterFunc(L, Utils.SETTER_IDX, "delayTime", _s_set_delayTime);
+            Utils.RegisterFunc(L, Utils.SETTER_IDX, "seted", _s_set_seted);
+            
+			Utils.EndObjectRegister(typeof(Delay), L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(typeof(Delay), L, __CreateInstance, 1, 0, 0);
+			
+			
+            
+            Utils.RegisterObject(L, translator, Utils.CLS_IDX, "UnderlyingSystemType", typeof(Delay));
+			
+			
+			Utils.EndClassRegister(typeof(Delay), L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+            ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			try {
+				if(LuaAPI.lua_gettop(L) == 1)
+				{
+					
+					Delay __cl_gen_ret = new Delay();
+					translator.Push(L, __cl_gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception __gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to Delay constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_ResetDelay(RealStatePtr L)
+        {
+            
+            ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+            Delay __cl_gen_to_be_invoked = (Delay)translator.FastGetCSObj(L, 1);
+            
+            
+            try {
+                
+                {
+                    float rootDelay = (float)LuaAPI.lua_tonumber(L, 2);
+                    
+                    __cl_gen_to_be_invoked.ResetDelay( rootDelay );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_delayTime(RealStatePtr L)
+        {
+            ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            try {
+			
+                Delay __cl_gen_to_be_invoked = (Delay)translator.FastGetCSObj(L, 1);
+                LuaAPI.lua_pushnumber(L, __cl_gen_to_be_invoked.delayTime);
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            return 1;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_seted(RealStatePtr L)
+        {
+            ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            try {
+			
+                Delay __cl_gen_to_be_invoked = (Delay)translator.FastGetCSObj(L, 1);
+                LuaAPI.lua_pushboolean(L, __cl_gen_to_be_invoked.seted);
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_delayTime(RealStatePtr L)
+        {
+            ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            try {
+			
+                Delay __cl_gen_to_be_invoked = (Delay)translator.FastGetCSObj(L, 1);
+                __cl_gen_to_be_invoked.delayTime = (float)LuaAPI.lua_tonumber(L, 2);
+            
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            return 0;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_seted(RealStatePtr L)
+        {
+            ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            try {
+			
+                Delay __cl_gen_to_be_invoked = (Delay)translator.FastGetCSObj(L, 1);
+                __cl_gen_to_be_invoked.seted = LuaAPI.lua_toboolean(L, 2);
+            
+            } catch(System.Exception __gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + __gen_e);
+            }
+            return 0;
+        }
+        
+		
+		
+		
+		
+    }
+}
